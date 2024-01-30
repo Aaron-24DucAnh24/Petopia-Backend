5abfdbec6a44291fc17cd91d956ac01f4b45737a
Minh Hung <acubu6969@gmail.com>
2024-01-30 22:33:11 +0700

    change enum 2

--- Petopia.Data/Enums/HouseType.cs
+++ Petopia.Data/Enums/HouseType.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Petopia.Data.Enums
 {
-    internal class HouseType
+    public enum HouseType
     {
+        Apartment,
+        House,
+        Dormitory,
+        Other
     }
 }
--- Petopia.Data/Enums/TakePetDuration.cs
+++ Petopia.Data/Enums/TakePetDuration.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Petopia.Data.Enums
 {
-    internal class TakePetDuration
+    public enum TakePetDuration
     {
+        Immediately,
+        FewDays,
+        OneWeek
     }
 }
